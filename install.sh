8a80c677d37581469aa816889b76bdee76c202a7
missmcady <67566759+missmcady@users.noreply.github.com>
2020-06-29 09:38:52 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ cd ${SH_PATH}
 
 create_mainfest_file(){
     echo "进行配置。。。"
-    read -p "请输入你的应用名称：" IBM_APP_NAME
+    read -p "请输入你的应用名称s：" IBM_APP_NAME
     echo "应用名称：${IBM_APP_NAME}"
     read -p "请输入你的应用内存大小(默认256)：" IBM_MEM_SIZE
     if [ -z "${IBM_MEM_SIZE}" ];then
